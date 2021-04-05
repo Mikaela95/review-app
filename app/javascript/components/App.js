580cfdf138e378067eb7b4e5353b0320158c21e9
mikaela.verhoosel <mikaela.verhoosel@gmail.com>
2021-04-06 08:11:09 +1000

    corrected file name

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,18 +2,22 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { Home } from './Home'
 import { About } from './About'
+import Jumbotron from "react-bootstrap/Jumbotron";
 
-function App() {
+const App = () => {
 	return (
 		<div>
 			<Switch>
-				<Route exact path="/" component={Home} />
-				<Route exact path="/about" component={About} />
+				<Jumbotron style={{ marginBottom: "auto" }}>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/about" component={About} />
+				</Jumbotron>
 			</Switch>
 		</div>
 	)
 }
 
 export default App;
+
 
 
