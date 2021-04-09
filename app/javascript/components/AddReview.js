92bd69880e234d84dfcae725f45ff37c6d8bf2c7
mikaela.verhoosel <mikaela.verhoosel@gmail.com>
2021-04-09 20:32:26 +1000

    can add a review - fixed CORS issue

--- app/javascript/components/AddReview.js
+++ app/javascript/components/AddReview.js
@@ -2,14 +2,95 @@ import React, { useState } from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import Alert from "react-bootstrap/Alert";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
 
 const AddReview = (props) => {
-  console.log(props)  
+	const [reviewsList, setReviewsList] = useState([]);
+	const [formState, setFormState] = useState({
+	    name: "",
+	    description: "",
+	    rating: "",
+  	});
+
+  	const handleReviewFormSubmit = (name, description, rating, restaurant) => {
+    
+    console.log("expense form submit test");
+
+    const newReview = {
+      name: name,
+      description: description,
+      rating: rating,
+      restaurant_id: restaurant,
+    };
+    
+    const newReviews = [...reviewsList];
+    newReviews.push(newReview);
+    setReviewsList(newReviews);
+
+    fetch("/api/v1/reviews", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newReview),
+    }).then((response) => {
+      console.log("use clases: response:", response);
+     // window.location.reload(false); 
+    });
+  };
+
+  const handleInputChange = (e) => {
+    const newState = { ...formState };
+    newState[e.target.name] = e.target.value;
+    setFormState(newState);
+  };
+  
+  const handleSubmit = (e) => {
+    //e.preventDefault();
+    handleReviewFormSubmit(
+      formState.name,
+      formState.description,
+      formState.rating,
+      props.data
+    );
+  };
 
   return (
-    <div>
-      <p>testing review component</p>
-    </div>
+    <Form onSubmit={handleSubmit}>
+      <Form.Row style={{ margin: "20px -5px" }}>
+        <Col xs={4}>
+          <Form.Control
+            placeholder="Your name"
+            name="name"
+            value={formState.name}
+            onChange={handleInputChange}
+            required
+          />
+        </Col>
+        <Col>
+          <Form.Control
+            placeholder="Description"
+            name="description"
+            value={formState.description}
+            onChange={handleInputChange}
+            required
+          />
+        </Col>
+        <Col>
+          <Form.Control
+            placeholder="Rating out of 5"
+            name="rating"
+            value={formState.rating}
+            onChange={handleInputChange}
+            required
+          />
+        </Col>
+        <Col>
+          <Button type="submit">Add Review</Button>
+        </Col>
+      </Form.Row>
+    </Form>
   );
 };
 
--- app/javascript/components/DeleteReview.js
+++ app/javascript/components/DeleteReview.js
@@ -4,11 +4,60 @@ import Button from "react-bootstrap/Button";
 import Alert from "react-bootstrap/Alert";
 
 const DeleteReview = (props) => {
-  console.log(props)  
+  const [modalShow, setModalShow] = useState(props.setModalShow);
+  const [show, setShow] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("handleSubmit");
+    console.log(props.reviewData.id);
+    handleDeleteExpense(props.reviewData.id);
+  };
+
+  const handleDeleteExpense = (review) => {
+    fetch(`/api/v1/reviews/${review}`, {
+      method: "DELETE",
+      body: JSON.stringify(review),
+    }).then((response) => {
+      console.log("DELETE response:", response);
+      window.location.reload(false);
+    });
+  };
+
+  const MyVerticallyCenteredModal = (props) => {
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Delete Review
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>
+            Are you sure you want to delete this review. This cannot be undone.
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={handleSubmit} variant="danger">
+            Confirm
+          </Button>
+          <Button onClick={props.onHide}>Cancel</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  };
 
   return (
     <div>
-      <p>testing delete component</p>
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
     </div>
   );
 };
